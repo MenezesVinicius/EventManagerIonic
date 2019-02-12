9c4eb0d8ff7c587fc629bb77933f050caffdb1cc
Vinicius <vicmen33@hotmail.com>
2019-02-12 04:52:15 -0300

    Autenticação: Login, Cadastro e Resetar senha funcionando com o firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,27 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Plugins} from '@capacitor/core';
+import * as firebase from 'firebase/app';
+import {firebaseConfig} from './credentials';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+const {SplashScreen, StatusBar} = Plugins;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html'
+    selector: 'app-root',
+    templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
+    constructor() {
+        firebase.initializeApp(firebaseConfig);
+        this.initializeApp();
+    }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+    initializeApp() {
+        SplashScreen.hide().catch(error => {
+            console.error(error);
+        });
+
+        StatusBar.hide().catch(error => {
+            console.error(error);
+        });
+    }
 }
---
+++ src/app/credentials.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyCN61XHNjN234YX36IhhnAXvnSvd2Ids2M',
+    authDomain: 'fir-codelab-vinicius.firebaseapp.com',
+    databaseURL: 'https://fir-codelab-vinicius.firebaseio.com',
+    projectId: 'fir-codelab-vinicius',
+    storageBucket: 'fir-codelab-vinicius.appspot.com',
+    messagingSenderId: '796651483751'
+};

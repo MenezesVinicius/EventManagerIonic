89f67eb697141cee318fff578d6797b30b0823ab
Vinicius Menezes <vicmen22@gmail.com>
2019-03-12 23:26:05 -0400

    Delete credentials.ts

--- src/app/credentials.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: 'AIzaSyCN61XHNjN234YX36IhhnAXvnSvd2Ids2M',
-    authDomain: 'fir-codelab-vinicius.firebaseapp.com',
-    databaseURL: 'https://fir-codelab-vinicius.firebaseio.com',
-    projectId: 'fir-codelab-vinicius',
-    storageBucket: 'fir-codelab-vinicius.appspot.com',
-    messagingSenderId: '796651483751'
-};
